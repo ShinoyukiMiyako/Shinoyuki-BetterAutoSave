6725a6019ecd222d5bde6c0f8f81f2745feb1924
xiaoxiao-cvs <2573270370@qq.com>
2026-05-04 02:13:12 +0800

    feat(config): 新增 ForgeConfigSpec 配置 schema
    定义 12 项 server 端配置, 涵盖节流预算, worker 池规模,
    关服超时, 重试次数, ChunkDataEvent.Save 兼容模式三档,
    以及诊断日志频率。
    
    BetterAutoSaveConfig 提供 typed getter 与 reload 监听,
    通过 volatile 变量缓存避免每次访问触发 ForgeConfigSpec
    内部 Locking。

--- src/main/java/com/shinoyuki/betterautosave/BetterAutoSaveMod.java
+++ src/main/java/com/shinoyuki/betterautosave/BetterAutoSaveMod.java
@@ -1,12 +1,16 @@
 package com.shinoyuki.betterautosave;
 
 import com.mojang.logging.LogUtils;
+import com.shinoyuki.betterautosave.config.BetterAutoSaveConfig;
+import com.shinoyuki.betterautosave.config.ConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -20,6 +24,9 @@ public final class BetterAutoSaveMod {
     public BetterAutoSaveMod() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         modBus.addListener(this::onCommonSetup);
+        modBus.addListener(BetterAutoSaveConfig::onLoad);
+        modBus.addListener(BetterAutoSaveConfig::onReload);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ConfigSpec.SPEC);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
